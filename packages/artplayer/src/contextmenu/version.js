42c4ca735d8baf498986ddc2eda5c1d9470d801c
Harvey Zhao <laozhaochaguan@gmail.com>
2026-08-31 16:59:25 +0800

    Update version.js

--- packages/artplayer/src/contextmenu/version.js
+++ packages/artplayer/src/contextmenu/version.js
@@ -1,10 +1,8 @@
 import { version } from '../../package.json'
-import { isBrowser } from '../utils/compatibility'
 
 export default function (option) {
-  const url = isBrowser ? location.href : ''
   return {
     ...option,
-    html: `<a href="https://artplayer.org?ref=${encodeURIComponent(url)}" target="_blank" style="width:100%;">ArtPlayer ${version}</a>`,
+    html: `<a href="https://artplayer.org" target="_blank" style="width:100%;">ArtPlayer ${version}</a>`,
   }
 }
